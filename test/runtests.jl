267fdcf34385abb0f482edce17b18daa0c78c053
nsaloua <58554997+nsaloua@users.noreply.github.com>
2020-06-16 14:11:16 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using Test_Optinum
 
+include("Algorithme_De_Newton.jl")
+include("Gradient_Conjugue_Tronque.jl")
+include("Lagrangien_Augmente.jl")
+include("Pas_De_Cauchy.jl")
+include("Regions_De_Confiance.jl")
+
+
+
 Test_Optinum.test_Algo_Newton(false,Algorithme_De_Newton)
 Test_Optinum.test_pas_de_cauchy(false,Pas_De_Cauchy)
 Test_Optinum.test_regions_de_confiance(false,Regions_De_Confiance)
