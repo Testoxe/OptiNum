5aca5b940d4b71a75af2b3535a3e39b9a924c861
relmo <elmontassir.rachid1937@gmail.com>
2020-06-16 17:12:46 +0200

    add deploydocs

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using Documenter
+using Sujet_Optinum
+
+makedocs(
+    modules = [Sujet_Optinum],
+    sitename = "Sujet_Optinum.jl",
+    strict=true,
+    authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
+            "Accueil" => "index.md"
+            ]
+    )
+
+deploydocs(
+    repo   = "github.com/mathn7/Sujet_Optinum.git",
+    branch = "gh-pages",  
+    devbranch = "master",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+# Optinum - Méthodes numériques pour les problèmes d’optimisation
+
+Le package [Optinum](https://github.com/mathn7/optinum) constitue une 
+solution du projet d’Optimisation Numérique pour l’année 2020-2021 de l'École INP-ENSEEIHT, 
+et contient tous les algorithmes vus en cours.
+
+###### Auteurs : O.Cots, J. Gergaud, S. Gratton, P. Matalon, C. Royer, D. Ruiz et E. Simon
