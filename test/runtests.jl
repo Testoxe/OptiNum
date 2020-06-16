5f6f06fab9a86d48b53c97b11956a12f4ab946ac
MohamedElWaghf <57914752+MohamedElWaghf@users.noreply.github.com>
2020-06-16 19:43:05 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,10 +10,47 @@ include("../src/Lagrangien_Augmente.jl")
 include("../src/Pas_De_Cauchy.jl")
 include("../src/Regions_De_Confiance.jl")
 
+printstyled("Lancement des tests sur ",bold=true,color=:white)
+printstyled("l'algorithme de Newton ",bold=true,color=:blue)
+println("")
+
+try
+    Test_Optinum.test_Algo_Newton(false,Algorithme_De_Newton)
+catch exception
+	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+end
 
 
-Test_Optinum.test_Algo_Newton(false,Algorithme_De_Newton)
-Test_Optinum.test_pas_de_cauchy(false,Pas_De_Cauchy)
-Test_Optinum.test_regions_de_confiance(false,Regions_De_Confiance)
-Test_Optinum.test_Lagrangien_Augmente(false,Lagrangien_Augmente)
+printstyled("Lancement des tests sur",bold=true,color=:white)
+printstyled(" le pas de cauchy ",bold=true,color=:blue)
+println("")
 
+try
+    Test_Optinum.test_pas_de_cauchy(false,Pas_De_Cauchy)
+catch exception
+	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+end
+
+
+printstyled("Lancement des tests sur",bold=true,color=:white)
+printstyled(" les régions de confiance ",bold=true,color=:blue)
+println("")
+
+
+try 
+    Test_Optinum.test_regions_de_confiance(false,Regions_De_Confiance)
+catch exception
+	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+end
+
+
+printstyled("Lancement des tests sur ",bold=true,color=:white)
+printstyled("le Lagrangien augmenté ",bold=true,color=:blue)
+println("")
+
+
+try
+    Test_Optinum.test_Lagrangien_Augmente(false,Lagrangien_Augmente)
+catch exception
+	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+end
