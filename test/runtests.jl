d1c8941a7f0ef8b5406a98adb6cf3dd3a2979074
relmo <elmontassir.rachid1937@gmail.com>
2020-06-18 22:39:12 +0200

    update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,54 +11,36 @@ include("../src/Pas_De_Cauchy.jl")
 include("../src/Regions_De_Confiance.jl")
 
 # Tester l'algorithme de Newton
-printstyled("Lancement des tests sur ",bold=true,color=:white)
-printstyled("l'algorithme de Newton ",bold=true,color=:blue)
-println("")
 try
 	Test_Optinum.tester_Algo_Newton(false,Algorithme_De_Newton)
-catch exception
-	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+catch e
+	printstyled("$e \n",bold=true,color=:red)
 end
 
-
 # Tester l'algorithme du pas de Cauchy
-printstyled("Lancement des tests sur",bold=true,color=:white)
-printstyled(" le pas de cauchy ",bold=true,color=:blue)
-println("")
 try
 	Test_Optinum.tester_pas_de_cauchy(false,Pas_De_Cauchy)
-catch exception
-	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+catch e
+	printstyled("$e \n",bold=true,color=:red)
 end
 
 # Tester l'algorithme du gradient conjugué tronqué
-printstyled("Lancement des tests sur",bold=true,color=:white)
-printstyled(" le gradient conjugué tronqué ",bold=true,color=:blue)
-println("")
 try
 	Test_Optinum.tester_gct(false,Gradient_Conjugue_Tronque)
-catch exception
-	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+catch e
+	printstyled("$e \n",bold=true,color=:red)
 end
-
 
 # Tester l'algorithme des Régions de confiance avec PasdeCauchy | GCT
-printstyled("Lancement des tests sur",bold=true,color=:white)
-printstyled(" les régions de confiance ",bold=true,color=:blue)
-println("")
 try 
 	Test_Optinum.tester_regions_de_confiance(false,Regions_De_Confiance)
-catch exception
-	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+catch e
+	printstyled("$e \n",bold=true,color=:red)
 end
 
-
 # Tester l'algorithme du Lagrangien Augmenté
-printstyled("Lancement des tests sur ",bold=true,color=:white)
-printstyled("le Lagrangien augmenté ",bold=true,color=:blue)
-println("")
 try
 	Test_Optinum.tester_Lagrangien_Augmente(false,Lagrangien_Augmente)
-catch exception
-	printstyled("$exception "*"\n"*"\n",bold=true,color=:red)
+catch e
+	printstyled("$e \n",bold=true,color=:red)
 end
