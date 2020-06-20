90b20d3b68da3ea4082515d29a47b76977ce2443
relmo <elmontassir.rachid1937@gmail.com>
2020-06-20 22:58:04 +0200

    adding try catch

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,21 +13,22 @@ include("../src/Regions_De_Confiance.jl")
 TestOptinum.cacher_stacktrace()
 
 # Tester l'algorithme de Newton
-res = @testset "Test Optinum" begin
-	
-	# Tester l'algorithme de Newton
-	TestOptinum.tester_Algo_Newton(false,Algorithme_De_Newton)
+try 
+	res = @testset "Test Optinum" begin		
+		# Tester l'algorithme de Newton
+		TestOptinum.tester_Algo_Newton(false,Algorithme_De_Newton)
 
-	# Tester l'algorithme du pas de Cauchy
-	TestOptinum.tester_pas_de_cauchy(false,Pas_De_Cauchy)
+		# Tester l'algorithme du pas de Cauchy
+		TestOptinum.tester_pas_de_cauchy(false,Pas_De_Cauchy)
 
-	# Tester l'algorithme du gradient conjugué tronqué
-	TestOptinum.tester_gct(false,Gradient_Conjugue_Tronque)
+		# Tester l'algorithme du gradient conjugué tronqué
+		TestOptinum.tester_gct(false,Gradient_Conjugue_Tronque)
 
-	# Tester l'algorithme des Régions de confiance avec PasdeCauchy | GCT
-	TestOptinum.tester_regions_de_confiance(false,Regions_De_Confiance)
+		# Tester l'algorithme des Régions de confiance avec PasdeCauchy | GCT
+		TestOptinum.tester_regions_de_confiance(false,Regions_De_Confiance)
 
-	# Tester l'algorithme du Lagrangien Augmenté
-	TestOptinum.tester_Lagrangien_Augmente(false,Lagrangien_Augmente)
-
+		# Tester l'algorithme du Lagrangien Augmenté
+		TestOptinum.tester_Lagrangien_Augmente(false,Lagrangien_Augmente)
+	end
+catch 
 end
